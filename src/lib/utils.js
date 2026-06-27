b6b86602f97156cb99b7ad1c91f5beef64299c41
enhanciar <enhanciar@gmail.com>
2026-06-27 22:12:09 +0530

    Add formatPrice helper for trek package prices

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+// Format a trek package price for display in INR.
+export function formatPrice(amount) {
+  return "₹" + amount.toLocaleString("en-IN");
+}
